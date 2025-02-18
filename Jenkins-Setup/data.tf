c8a7074eb81aa4eae9b8e307de6ef5f3831152b9
omkara <omkarshelke1125@gmail.com>
2025-02-18 19:13:58 +0530

    data source block update for ami

---
+++ Jenkins-Setup/data.tf
@@ -0,0 +1,19 @@
+
+data "aws_ami" "image_" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+output "ami_id" {
+  value = data.aws_ami.image.id
+}
