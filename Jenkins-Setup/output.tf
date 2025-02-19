9b77fdcbd0411c5822ecac3c5230b21388db64e9
omkara <omkarshelke1125@gmail.com>
2025-02-19 19:17:46 +0530

    ssh out block updated

--- Jenkins-Setup/output.tf
+++ Jenkins-Setup/output.tf
@@ -8,5 +8,25 @@ output "jenkins_slave_public_ip" {
   value       = aws_instance.jenkins_slave.public_ip
 }
 
+output "jenkins_master_url" {
+  description = "Jenkins Web Interface URL"
+  value       = "http://${aws_instance.jenkins_master.public_ip}:8080"
+}
 
+output "jenkins_initial_admin_password" {
+  description = "Jenkins Initial Admin Password"
+  value       = <<EOT
+  Run the following command to get the password:
+  ssh -i jenkins-login.pem ec2-user@${aws_instance.jenkins_master.public_ip} "cat /tmp/jenkins_admin_password"
+  EOT
+}
 
+output "ssh_command_master" {
+  description = "SSH command to connect to the Jenkins Master instance"
+  value       = "ssh -i jenkins-login.pem ec2-user@${aws_instance.jenkins_master.public_ip}"
+}
+
+output "ssh_command_slave" {
+  description = "SSH command to connect to the Jenkins Slave instance"
+  value       = "ssh -i jenkins-login.pem ec2-user@${aws_instance.jenkins_slave.public_ip}"
+}
